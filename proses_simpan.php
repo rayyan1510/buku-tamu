65c23c094d4ce68f0ffde9b75eb8fc0a1a94841b
Farhan R. <farhanraditiansyah@gmail.com>
2025-01-16 12:19:49 +0700

    Logic On Progress
    Masih bermasalah di logic input

--- proses_simpan.php
+++ proses_simpan.php
@@ -1,6 +1,6 @@
 <?php
 
-// include penyimpanand
+include 'connection.php';
 
 var_dump($_POST);
 
@@ -8,12 +8,26 @@ var_dump($_POST);
 // cek jika ada di set $_post
 if (isset($_POST) == true) {
 
-    $nama_tamu = htmlspecialchars($_POST['nama_tamu']);
-    $no_telp = htmlspecialchars($_POST['no_telp']);
+    $nama_tamu = $_POST['nama'];
+    $no_telp = $_POST['nohp'];
+    $pekerjaan = $_POST['pekerjaan'];
+    // $detailPekerjaan = isset($_POST['detailPekerjaan']) ? $_POST['detailPekerjaan'] : null;
+    // $detailPekerjaanSpesifik = isset($_POST['detailPekerjaanSpesifik']) ? $_POST['detailPekerjaanSpesifik'] : null;
+    $keperluan = $_POST['keperluan'];
+    // $keperluanInput = isset($_POST['keperluanInput']) ? $_POST['keperluanInput'] : null;
 
-    $$pekerjaan;
+    // Prepare and bind
+    $stmt = $koneksi->prepare("INSERT INTO tbl_tamu (nama_tamu, nomor_hp, jenis_tamu, keperluan_tamu) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("", $nama_tamu, $no_telp, $pekerjaan, $keperluan);
 
-    echo "<br>true";
-} else {
-    echo "<br>true";
+    // Execute the statement
+    if ($stmt->execute()) {
+        echo "Data berhasil disimpan!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the statement and connection
+    $stmt->close();
+    $conn->close();
 }
